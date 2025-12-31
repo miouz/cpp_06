8111dbe8e3b64ac2b730d088fc99a66df0d5c88f
Zhou Mi <mizhouis@gmail.com>
2025-12-31 13:30:03 +0100

    add(ex00): add files for ex00

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,15 @@
+#ifndef SCALARCONVERT_HPP_
+#define SCALARCONVERT_HPP_
+
+#include <string>
+
+class ScalarConverter
+{
+	private:
+	ScalarConverter();
+
+	public:
+	static void convert(const std::string& ref);
+};
+
+#endif // !SCALARCONVERT_HPP_
