df2dd42a74ca2ac1fd0c806d7513adc97aa6cd6a
Mi Zhou <mzhou@student.42.fr>
2026-01-06 12:12:06 +0100

    add(ex00): add test suit files

---
+++ ex00/test.sh
@@ -0,0 +1,17 @@
+#! bin/bash
+
+#color variables
+RED := \033[0;31m
+GREEN := \033[0;32m
+YELLOW := \033[0;33m
+BLUE := \033[0;34m
+MAGENTA := \033[0;35m
+CYAN := \033[0;36m
+WHITE := \033[0;37m
+RESET := \033[0m
+BOLD := \033[1m
+
+PROGRAMME=./convert
+TEST_PASSED=0
+TEST_FAILED=0
+
