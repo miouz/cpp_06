219e05e88d6016957b9e63aa6166d2ba9b9a22c7
Mi Zhou <mzhou@student.42.fr>
2026-01-06 16:31:24 +0100

    update(ex00): update test file

--- ex00/test.sh
+++ ex00/test.sh
@@ -19,11 +19,59 @@ fi
 
 run_test(){
 	local input="$1"
+	local description="$2"
+	local output=$($PROGRAM "$input" | tr '\n' ' ')
 
-	printf "${YELLOW}Running test ${GREEN}[$input] \n${RESET}"
-	printf "${YELLOW}Result: \n${RESET}"
-	$PROGRAM "$input"
+	printf "${YELLOW}Test: ${GREEN}[$input] Case: [$description]\n${RESET}"
+	printf "$output \n"
 	printf "\n"
 }
 
-run_test "a"
+printf "${GREEN}==========ARGUMENTS TEST=============${RESET}\n"
+run_test "" "empty string test"
+
+
+printf "${GREEN}==========INVALID INPUT TEST=============${RESET}\n"
+run_test "++123" "multiple +"
+run_test "-01ab" "mixed input"
+run_test "42ff" "multiple f"
+run_test "42.42.42.42" "multiple dots"
+
+printf "${GREEN}==========CHAR TEST=============${RESET}\n"
+run_test "a" "normal displayable char"
+run_test "Z" "normal displayable char"
+run_test "33" "first displayable char <!>"
+run_test "126" "last displayable char <~>"
+run_test "127" "last nondisplayable char <DEL>"
+run_test "32" "nondisplayable char <SPACE>"
+run_test "0" "nondisplayable char <NULL>"
+run_test "128" "impossible char <out of range>"
+run_test "-1" "impossible char <out of range>"
+
+printf "${GREEN}==========INT TEST=============${RESET}\n"
+run_test "2147483647" "int max"
+run_test "-2147483648" "int min"
+run_test "2147483648" "int max + 1"
+run_test "-2147483649" "int min - 1"
+
+printf "${GREEN}==========FLOAT TEST=============${RESET}\n"
+run_test "0f" "integer.0f display"
+run_test "42f" "integer.0f display"
+run_test "3.40282e+38" "Float MAX (scientific notation)"
+run_test "-3.40282e+38" "Float MIN (scientific notation)"
+run_test "3.40282347e+38f" "Float MAX with f suffix"
+run_test "1e39" "Beyond float max, within double"
+run_test "3.5e38" "Slightly above float max"
+
+printf "${GREEN}==========DOUBLE TEST=============${RESET}\n"
+run_test "1e400" "Huge value - exceeds double"
+run_test "3.1234567890123456789" "loss precision"
+
+printf "${GREEN}==========SPECIAL LITERAL TEST=============${RESET}\n"
+run_test "inff" "with f"
+run_test "+inff" "with f"
+run_test "-inff" "with f"
+run_test "inf" "without f"
+run_test "+inf" "without f"
+run_test "nan" "without f"
+run_test "nanf" "with f"
