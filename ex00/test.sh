8e4d9bcb21cd43e91be673450aa6403e92618d9e
Mi Zhou <mzhou@student.42.fr>
2026-01-06 14:26:52 +0100

    add(ex00): add working test suits

--- ex00/test.sh
+++ ex00/test.sh
@@ -1,17 +1,29 @@
-#! bin/bash
+#!/bin/bash
 
 #color variables
-RED := \033[0;31m
-GREEN := \033[0;32m
-YELLOW := \033[0;33m
-BLUE := \033[0;34m
-MAGENTA := \033[0;35m
-CYAN := \033[0;36m
-WHITE := \033[0;37m
-RESET := \033[0m
-BOLD := \033[1m
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+RESET='\033[0m'
 
-PROGRAMME=./convert
-TEST_PASSED=0
-TEST_FAILED=0
+PROGRAM="./convert"
 
+if [ ! -f $PROGRAM ]; then
+	printf "${RED} Error: $PROGRAM do not exit\n${RESET}"
+	exit 1
+fi 
+
+run_test(){
+	local input="$1"
+
+	printf "${YELLOW}Running test ${GREEN}[$input] \n${RESET}"
+	printf "${YELLOW}Result: \n${RESET}"
+	$PROGRAM "$input"
+	printf "\n"
+}
+
+run_test "a"
