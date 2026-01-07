ed4ce421c289bd4eb53ab1aa83b6f913167be9f9
Zhou Mi <mizhouis@gmail.com>
2026-01-07 15:58:56 +0100

    update(ex00): add canonic form to class

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -23,6 +23,9 @@ class ScalarConverter
 {
 	private:
 	ScalarConverter();
+	ScalarConverter(const ScalarConverter& other);
+	ScalarConverter& operator=(const ScalarConverter& other);
+	~ScalarConverter();
 
 	public:
 	static void convert(const std::string& literal);
